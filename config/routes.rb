15abad4a88fdbc4b01b79cd6444e6b858a3de404
hys-ch <108731664+hys-ch@users.noreply.github.com>
2022-08-01 22:43:31 +0000

    〔Add〕詳細画面

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,15 @@ Rails.application.routes.draw do
   get 'lists' => 'lists#index'
 end
 
-  get 'lists/show'
+Rails.application.routes.draw do
+  get 'lists/new'
+  get 'lists/edit'
+  get 'top' => 'homes#top'
+  post 'lists' => 'lists#create'
+  get 'lists' => 'lists#index'
+  get 'lists/:id' => 'lists#show', as: 'list'
+end
+
   get 'lists/edit'
   get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
